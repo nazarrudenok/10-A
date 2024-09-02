655fa319bf6d97e6577fc345d75c5bb63c4519b4
Nazar Rudenok <122445773+nazarrudenok@users.noreply.github.com>
2024-09-02 19:21:27 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,184 @@
+function mon() {
+    const value = document.getElementsByClassName('mon')[0];
+    value.style.background = '#7c7bff';
+    value.style.borderColor = '#7c7bff';
+    const scheduleCont = document.querySelector('.schedule-cont');
+    scheduleCont.innerHTML = `
+            <div class="lesson">
+                <p class="num">1</p>
+                <p class="value"></p>
+            </div>
+            <div class="lesson">
+                <p class="num">2</p>
+                <p class="value">Фізична культура</p>
+            </div>
+            <div class="lesson">
+                <p class="num">3</p>
+                <p class="value">Історія України</p>
+            </div>
+            <div class="lesson">
+                <p class="num">4</p>
+                <p class="value">Зарубіжна література</p>
+            </div>
+            <div class="lesson">
+                <p class="num">5</p>
+                <p class="value"></p>
+            </div>
+            <div class="lesson">
+                <p class="num">6</p>
+                <p class="value"></p>
+            </div>
+            <div class="lesson">
+                <p class="num">7</p>
+                <p class="value"></p>
+            </div>
+    `;
+}
+
+function tue() {
+    const value = document.getElementsByClassName('tue')[0];
+    value.style.background = '#7c7bff';
+    value.style.borderColor = '#7c7bff';
+    const scheduleCont = document.querySelector('.schedule-cont');
+    scheduleCont.innerHTML = `
+            <div class="lesson">
+                <p class="num">1</p>
+                <p class="value">Історія України</p>
+            </div>
+            <div class="lesson">
+                <p class="num">2</p>
+                <p class="value">Фізика</p>
+            </div>
+            <div class="lesson">
+                <p class="num">3</p>
+                <p class="value">Біологія</p>
+            </div>
+            <div class="lesson">
+                <p class="num">4</p>
+                <p class="value">Біологія</p>
+            </div>
+            <div class="lesson">
+                <p class="num">5</p>
+                <p class="value">Українська література</p>
+            </div>
+            <div class="lesson">
+                <p class="num">6</p>
+                <p class="value">Алгебра</p>
+            </div>
+            <div class="lesson">
+                <p class="num">7</p>
+                <p class="value">Фізична культура</p>
+            </div>
+    `;
+}
+
+function wed() {
+    const value = document.getElementsByClassName('wed')[0];
+    value.style.background = '#7c7bff';
+    value.style.borderColor = '#7c7bff';
+    const scheduleCont = document.querySelector('.schedule-cont');
+    scheduleCont.innerHTML = `
+            <div class="lesson">
+                <p class="num">1</p>
+                <p class="value"></p>
+            </div>
+            <div class="lesson">
+                <p class="num">2</p>
+                <p class="value">Фізика</p>
+            </div>
+            <div class="lesson">
+                <p class="num">3</p>
+                <p class="value">Економіка</p>
+            </div>
+            <div class="lesson">
+                <p class="num">4</p>
+                <p class="value">Українська мова</p>
+            </div>
+            <div class="lesson">
+                <p class="num">5</p>
+                <p class="value">Англійська мова</p>
+            </div>
+            <div class="lesson">
+                <p class="num">6</p>
+                <p class="value"></p>
+            </div>
+            <div class="lesson">
+                <p class="num">7</p>
+                <p class="value"></p>
+            </div>
+    `;
+}
+
+function thu() {
+    const value = document.getElementsByClassName('thu')[0];
+    value.style.background = '#7c7bff';
+    value.style.borderColor = '#7c7bff';
+    const scheduleCont = document.querySelector('.schedule-cont');
+    scheduleCont.innerHTML = `
+            <div class="lesson">
+                <p class="num">1</p>
+                <p class="value"></p>
+            </div>
+            <div class="lesson">
+                <p class="num">2</p>
+                <p class="value"></p>
+            </div>
+            <div class="lesson">
+                <p class="num">3</p>
+                <p class="value"></p>
+            </div>
+            <div class="lesson">
+                <p class="num">4</p>
+                <p class="value"></p>
+            </div>
+            <div class="lesson">
+                <p class="num">5</p>
+                <p class="value"></p>
+            </div>
+            <div class="lesson">
+                <p class="num">6</p>
+                <p class="value"></p>
+            </div>
+            <div class="lesson">
+                <p class="num">7</p>
+                <p class="value"></p>
+            </div>
+    `;
+}
+
+function fri() {
+    const value = document.getElementsByClassName('fri')[0];
+    value.style.background = '#7c7bff';
+    value.style.borderColor = '#7c7bff';
+    const scheduleCont = document.querySelector('.schedule-cont');
+    scheduleCont.innerHTML = `
+            <div class="lesson">
+                <p class="num">1</p>
+                <p class="value"></p>
+            </div>
+            <div class="lesson">
+                <p class="num">2</p>
+                <p class="value"></p>
+            </div>
+            <div class="lesson">
+                <p class="num">3</p>
+                <p class="value"></p>
+            </div>
+            <div class="lesson">
+                <p class="num">4</p>
+                <p class="value"></p>
+            </div>
+            <div class="lesson">
+                <p class="num">5</p>
+                <p class="value"></p>
+            </div>
+            <div class="lesson">
+                <p class="num">6</p>
+                <p class="value"></p>
+            </div>
+            <div class="lesson">
+                <p class="num">7</p>
+                <p class="value"></p>
+            </div>
+    `;
+}
