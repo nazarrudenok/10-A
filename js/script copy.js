06e897583a879b0685e1bcd9966d1034c8ac3f5e
Nazar Rudenok <122445773+nazarrudenok@users.noreply.github.com>
2024-09-10 20:00:48 +0300

    Add files via upload

---
+++ js/script copy.js
@@ -0,0 +1,253 @@
+function mon() {
+    const scheduleCont = document.querySelector('.schedule-cont');
+    scheduleCont.innerHTML = `
+            <div class="lesson">
+                <div class="time">
+                    <p class="first">8:00</p>
+                    <p class="line">-</p>
+                    <p class="second">8:45</p>
+                </div>
+                <p class="value">Економіка</p>
+            </div>
+            <div class="lesson">
+                <div class="time">
+                    <p class="first">8:50</p>
+                    <p class="line">-</p>
+                    <p class="second">9:35</p>
+                </div>
+                <p class="value">Економіка</p>
+            </div>
+            <div class="lesson">
+                <div class="time">
+                    <p class="first">9:45</p>
+                    <p class="line">-</p>
+                    <p class="second">11:30</p>
+                </div>
+                <p class="value">Громадянська освіта</p>
+            </div>
+            <div class="lesson">
+                <div class="time">
+                    <p class="first">11:40</p>
+                    <p class="line">-</p>
+                    <p class="second">12:25</p>
+                </div>
+                <p class="value">Геометрія</p>
+            </div>
+            <div class="lesson">
+                <div class="time">
+                    <p class="first">8:00</p>
+                    <p class="line">-</p>
+                    <p class="second">8:45</p>
+                </div>
+                <p class="value">Англійська мова</p>
+            </div>
+            <div class="lesson">
+                <div class="time">
+                    <p class="first">12:35</p>
+                    <p class="line">-</p>
+                    <p class="second">13:20</p>
+                </div>
+                <p class="value">Українська мова</p>
+            </div>
+            <div class="lesson">
+                <div class="time">
+                    <p class="first">13:30</p>
+                    <p class="line">-</p>
+                    <p class="second">14:15</p>
+                </div>
+                <p class="value"></p>
+            </div>
+    `;
+}
+
+function tue() {
+    const scheduleCont = document.querySelector('.schedule-cont');
+    scheduleCont.innerHTML = `
+            <div class="lesson">
+                <p class="num">1</p>
+                <p class="value">Алгебра</p>
+            </div>
+            <div class="lesson">
+                <p class="num">2</p>
+                <p class="value">Фізика</p>
+            </div>
+            <div class="lesson">
+                <p class="num">3</p>
+                <p class="value">Біологія</p>
+            </div>
+            <div class="lesson">
+                <p class="num">4</p>
+                <p class="value">Біологія</p>
+            </div>
+            <div class="lesson">
+                <p class="num">5</p>
+                <p class="value">Українська література</p>
+            </div>
+            <div class="lesson">
+                <p class="num">6</p>
+                <p class="value">Фізична культура</p>
+            </div>
+            <div class="lesson">
+                <p class="num">7</p>
+                <p class="value">Історія України</p>
+            </div>
+    `;
+}
+
+function wed() {
+    const scheduleCont = document.querySelector('.schedule-cont');
+    scheduleCont.innerHTML = `
+            <div class="lesson">
+                <p class="num">1</p>
+                <p class="value">Алгебра</p>
+            </div>
+            <div class="lesson">
+                <p class="num">2</p>
+                <p class="value">Фізика</p>
+            </div>
+            <div class="lesson">
+                <p class="num">3</p>
+                <p class="value">Економіка</p>
+            </div>
+            <div class="lesson">
+                <p class="num">4</p>
+                <p class="value">Українська мова</p>
+            </div>
+            <div class="lesson">
+                <p class="num">5</p>
+                <p class="value">Англійська мова</p>
+            </div>
+            <div class="lesson">
+                <p class="num">6</p>
+                <p class="value"></p>
+            </div>
+            <div class="lesson">
+                <p class="num">7</p>
+                <p class="value"></p>
+            </div>
+    `;
+}
+
+function thu() {
+    const scheduleCont = document.querySelector('.schedule-cont');
+    scheduleCont.innerHTML = `
+            <div class="lesson">
+                <p class="num">1</p>
+                <p class="value">Алгебра</p>
+            </div>
+            <div class="lesson">
+                <p class="num">2</p>
+                <p class="value">Алгебра</p>
+            </div>
+            <div class="lesson">
+                <p class="num">3</p>
+                <p class="value">Зарубіжна література</p>
+            </div>
+            <div class="lesson">
+                <p class="num">4</p>
+                <p class="value">Фізична культура</p>
+            </div>
+            <div class="lesson">
+                <p class="num">5</p>
+                <p class="value">Громадянська освіта</p>
+            </div>
+            <div class="lesson">
+                <p class="num">6</p>
+                <p class="value">Географія</p>
+            </div>
+            <div class="lesson">
+                <p class="num">7</p>
+                <p class="value">Географія</p>
+            </div>
+    `;
+}
+
+function fri() {
+    const scheduleCont = document.querySelector('.schedule-cont');
+    scheduleCont.innerHTML = `
+            <div class="lesson">
+                <p class="num">1</p>
+                <p class="value"></p>
+            </div>
+            <div class="lesson">
+                <p class="num">2</p>
+                <p class="value">Фізика</p>
+            </div>
+            <div class="lesson">
+                <p class="num">3</p>
+                <p class="value">Технології</p>
+            </div>
+            <div class="lesson">
+                <p class="num">4</p>
+                <p class="value">Інформатика</p>
+            </div>
+            <div class="lesson">
+                <p class="num">5</p>
+                <p class="value">Інформатика</p>
+            </div>
+            <div class="lesson">
+                <p class="num">6</p>
+                <p class="value">Українська література</p>
+            </div>
+            <div class="lesson">
+                <p class="num">7</p>
+                <p class="value">Всесвітня історія</p>
+            </div>
+    `;
+}
+
+function first() {
+    const scheduleCont = document.querySelector('.schedule-cont');
+    scheduleCont.innerHTML = `
+            <div class="lesson">
+                <p class="num">1</p>
+                <p class="value">08:00 - 08:45</p>
+            </div>
+            <div class="lesson">
+                <p class="num">2</p>
+                <p class="value">08:50 - 09:35</p>
+            </div>
+            <div class="lesson">
+                <p class="num">3</p>
+                <p class="value">09:45 - 10:30</p>
+            </div>
+            <div class="lesson">
+                <p class="num">4</p>
+                <p class="value">10:45 - 11:30</p>
+            </div>
+            <div class="lesson">
+                <p class="num">5</p>
+                <p class="value">11:40 - 12:25</p>
+            </div>
+            <div class="lesson">
+                <p class="num">6</p>
+                <p class="value">12:35 - 13:20</p>
+            </div>
+            <div class="lesson">
+                <p class="num">7</p>
+                <p class="value">13:30 - 14:15</p>
+            </div>
+    `;
+}
+
+function second() {
+    const scheduleCont = document.querySelector('.schedule-cont');
+    scheduleCont.innerHTML = `
+            <div class="lesson">
+                <p class="num">1</p>
+                <p class="value">08:30 - 09:50</p>
+            </div>
+            <div class="lesson">
+                <p class="num">2</p>
+                <p class="value">10:05 - 11:25</p>
+            </div>
+            <div class="lesson">
+                <p class="num">3</p>
+                <p class="value">11:50 - 13:05</p>
+            </div>
+            <div class="lesson">
+                <p class="num">4</p>
+                <p class="value">13:20 - 14:40</p>
+            </div>
+    `;
+}
--- js/script.js
+++ js/script.js
@@ -2,33 +2,61 @@ function mon() {
     const scheduleCont = document.querySelector('.schedule-cont');
     scheduleCont.innerHTML = `
             <div class="lesson">
-                <p class="num">1</p>
+                <div class="time">
+                    <p class="first">8:00</p>
+                    <p class="line">-</p>
+                    <p class="second">8:45</p>
+                </div>
                 <p class="value">Економіка</p>
             </div>
             <div class="lesson">
-                <p class="num">2</p>
+                <div class="time">
+                    <p class="first">8:50</p>
+                    <p class="line">-</p>
+                    <p class="second">9:35</p>
+                </div>
                 <p class="value">Економіка</p>
             </div>
             <div class="lesson">
-                <p class="num">3</p>
+                <div class="time">
+                    <p class="first">9:45</p>
+                    <p class="line">-</p>
+                    <p class="second">10:30</p>
+                </div>
                 <p class="value">Громадянська освіта</p>
             </div>
             <div class="lesson">
-                <p class="num">4</p>
+                <div class="time">
+                    <p class="first">10:45</p>
+                    <p class="line">-</p>
+                    <p class="second">11:30</p>
+                </div>
                 <p class="value">Геометрія</p>
             </div>
             <div class="lesson">
-                <p class="num">5</p>
+                <div class="time">
+                    <p class="first">11:40</p>
+                    <p class="line">-</p>
+                    <p class="second">12:25</p>
+                </div>
                 <p class="value">Англійська мова</p>
             </div>
             <div class="lesson">
-                <p class="num">6</p>
+                <div class="time">
+                    <p class="first">12:35</p>
+                    <p class="line">-</p>
+                    <p class="second">13:30</p>
+                </div>
                 <p class="value">Українська мова</p>
             </div>
-            <div class="lesson">
-                <p class="num">7</p>
+            <!-- <div class="lesson">
+                <div class="time">
+                    <p class="first">13:30</p>
+                    <p class="line">-</p>
+                    <p class="second">14:15</p>
+                </div>
                 <p class="value"></p>
-            </div>
+            </div> -->
     `;
 }
 
@@ -36,31 +64,59 @@ function tue() {
     const scheduleCont = document.querySelector('.schedule-cont');
     scheduleCont.innerHTML = `
             <div class="lesson">
-                <p class="num">1</p>
+                <div class="time">
+                    <p class="first">8:00</p>
+                    <p class="line">-</p>
+                    <p class="second">8:45</p>
+                </div>
                 <p class="value">Алгебра</p>
             </div>
             <div class="lesson">
-                <p class="num">2</p>
+                <div class="time">
+                    <p class="first">8:50</p>
+                    <p class="line">-</p>
+                    <p class="second">9:35</p>
+                </div>
                 <p class="value">Фізика</p>
             </div>
             <div class="lesson">
-                <p class="num">3</p>
+                <div class="time">
+                    <p class="first">9:45</p>
+                    <p class="line">-</p>
+                    <p class="second">10:30</p>
+                </div>
                 <p class="value">Біологія</p>
             </div>
             <div class="lesson">
-                <p class="num">4</p>
+                <div class="time">
+                    <p class="first">10:45</p>
+                    <p class="line">-</p>
+                    <p class="second">11:30</p>
+                </div>
                 <p class="value">Біологія</p>
             </div>
             <div class="lesson">
-                <p class="num">5</p>
+                <div class="time">
+                    <p class="first">11:40</p>
+                    <p class="line">-</p>
+                    <p class="second">12:25</p>
+                </div>
                 <p class="value">Українська література</p>
             </div>
             <div class="lesson">
-                <p class="num">6</p>
+                <div class="time">
+                    <p class="first">12:35</p>
+                    <p class="line">-</p>
+                    <p class="second">13:30</p>
+                </div>
                 <p class="value">Фізична культура</p>
             </div>
             <div class="lesson">
-                <p class="num">7</p>
+                <div class="time">
+                    <p class="first">13:40</p>
+                    <p class="line">-</p>
+                    <p class="second">14:15</p>
+                </div>
                 <p class="value">Історія України</p>
             </div>
     `;
@@ -70,32 +126,36 @@ function wed() {
     const scheduleCont = document.querySelector('.schedule-cont');
     scheduleCont.innerHTML = `
             <div class="lesson">
-                <p class="num">1</p>
+                <div class="time">
+                    <p class="first">8:00</p>
+                    <p class="line">-</p>
+                    <p class="second">8:45</p>
+                </div>
                 <p class="value">Алгебра</p>
             </div>
             <div class="lesson">
-                <p class="num">2</p>
+                <div class="time">
+                    <p class="first">8:50</p>
+                    <p class="line">-</p>
+                    <p class="second">9:35</p>
+                </div>
                 <p class="value">Фізика</p>
             </div>
             <div class="lesson">
-                <p class="num">3</p>
+                <div class="time">
+                    <p class="first">9:45</p>
+                    <p class="line">-</p>
+                    <p class="second">10:30</p>
+                </div>
                 <p class="value">Економіка</p>
             </div>
             <div class="lesson">
-                <p class="num">4</p>
+                <div class="time">
+                    <p class="first">10:45</p>
+                    <p class="line">-</p>
+                    <p class="second">11:30</p>
+                </div>
                 <p class="value">Українська мова</p>
-            </div>
-            <div class="lesson">
-                <p class="num">5</p>
-                <p class="value">Англійська мова</p>
-            </div>
-            <div class="lesson">
-                <p class="num">6</p>
-                <p class="value"></p>
-            </div>
-            <div class="lesson">
-                <p class="num">7</p>
-                <p class="value"></p>
             </div>
     `;
 }
@@ -104,31 +164,59 @@ function thu() {
     const scheduleCont = document.querySelector('.schedule-cont');
     scheduleCont.innerHTML = `
             <div class="lesson">
-                <p class="num">1</p>
+                <div class="time">
+                    <p class="first">8:00</p>
+                    <p class="line">-</p>
+                    <p class="second">8:45</p>
+                </div>
                 <p class="value">Алгебра</p>
             </div>
             <div class="lesson">
-                <p class="num">2</p>
+                <div class="time">
+                    <p class="first">8:50</p>
+                    <p class="line">-</p>
+                    <p class="second">9:35</p>
+                </div>
                 <p class="value">Алгебра</p>
             </div>
             <div class="lesson">
-                <p class="num">3</p>
+                <div class="time">
+                    <p class="first">9:45</p>
+                    <p class="line">-</p>
+                    <p class="second">10:30</p>
+                </div>
                 <p class="value">Зарубіжна література</p>
             </div>
             <div class="lesson">
-                <p class="num">4</p>
+                <div class="time">
+                    <p class="first">10:45</p>
+                    <p class="line">-</p>
+                    <p class="second">11:30</p>
+                </div>
                 <p class="value">Фізична культура</p>
             </div>
             <div class="lesson">
-                <p class="num">5</p>
+                <div class="time">
+                    <p class="first">11:40</p>
+                    <p class="line">-</p>
+                    <p class="second">12:25</p>
+                </div>
                 <p class="value">Громадянська освіта</p>
             </div>
             <div class="lesson">
-                <p class="num">6</p>
+                <div class="time">
+                    <p class="first">12:35</p>
+                    <p class="line">-</p>
+                    <p class="second">13:30</p>
+                </div>
                 <p class="value">Географія</p>
             </div>
             <div class="lesson">
-                <p class="num">7</p>
+                <div class="time">
+                    <p class="first">13:40</p>
+                    <p class="line">-</p>
+                    <p class="second">14:15</p>
+                </div>
                 <p class="value">Географія</p>
             </div>
     `;
@@ -138,31 +226,59 @@ function fri() {
     const scheduleCont = document.querySelector('.schedule-cont');
     scheduleCont.innerHTML = `
             <div class="lesson">
-                <p class="num">1</p>
+                <div class="time">
+                    <p class="first">8:00</p>
+                    <p class="line">-</p>
+                    <p class="second">8:45</p>
+                </div>
                 <p class="value"></p>
             </div>
             <div class="lesson">
-                <p class="num">2</p>
+                <div class="time">
+                    <p class="first">8:50</p>
+                    <p class="line">-</p>
+                    <p class="second">9:35</p>
+                </div>
                 <p class="value">Фізика</p>
             </div>
             <div class="lesson">
-                <p class="num">3</p>
+                <div class="time">
+                    <p class="first">9:45</p>
+                    <p class="line">-</p>
+                    <p class="second">10:30</p>
+                </div>
                 <p class="value">Технології</p>
             </div>
             <div class="lesson">
-                <p class="num">4</p>
+                <div class="time">
+                    <p class="first">10:45</p>
+                    <p class="line">-</p>
+                    <p class="second">11:30</p>
+                </div>
                 <p class="value">Інформатика</p>
             </div>
             <div class="lesson">
-                <p class="num">5</p>
+                <div class="time">
+                    <p class="first">11:40</p>
+                    <p class="line">-</p>
+                    <p class="second">12:25</p>
+                </div>
                 <p class="value">Інформатика</p>
             </div>
             <div class="lesson">
-                <p class="num">6</p>
+                <div class="time">
+                    <p class="first">12:35</p>
+                    <p class="line">-</p>
+                    <p class="second">13:30</p>
+                </div>
                 <p class="value">Українська література</p>
             </div>
             <div class="lesson">
-                <p class="num">7</p>
+                <div class="time">
+                    <p class="first">13:40</p>
+                    <p class="line">-</p>
+                    <p class="second">14:15</p>
+                </div>
                 <p class="value">Всесвітня історія</p>
             </div>
     `;
@@ -172,31 +288,52 @@ function first() {
     const scheduleCont = document.querySelector('.schedule-cont');
     scheduleCont.innerHTML = `
             <div class="lesson">
-                <p class="num">1</p>
+                <div class="time">
+                    <p class="first">1</p>
+                    <p class="line">урок</p>
+                </div>
                 <p class="value">08:00 - 08:45</p>
             </div>
             <div class="lesson">
-                <p class="num">2</p>
+                <div class="time">
+                    <p class="first">2</p>
+                    <p class="line">урок</p>
+                </div>
                 <p class="value">08:50 - 09:35</p>
             </div>
             <div class="lesson">
-                <p class="num">3</p>
+                <div class="time">
+                    <p class="first">3</p>
+                    <p class="line">урок</p>
+                </div>
                 <p class="value">09:45 - 10:30</p>
             </div>
             <div class="lesson">
-                <p class="num">4</p>
+                <div class="time">
+                    <p class="first">4</p>
+                    <p class="line">урок</p>
+                </div>
                 <p class="value">10:45 - 11:30</p>
             </div>
             <div class="lesson">
-                <p class="num">5</p>
+                <div class="time">
+                    <p class="first">5</p>
+                    <p class="line">урок</p>
+                </div>
                 <p class="value">11:40 - 12:25</p>
             </div>
             <div class="lesson">
-                <p class="num">6</p>
+                <div class="time">
+                    <p class="first">6</p>
+                    <p class="line">урок</p>
+                </div>
                 <p class="value">12:35 - 13:20</p>
             </div>
             <div class="lesson">
-                <p class="num">7</p>
+                <div class="time">
+                    <p class="first">7</p>
+                    <p class="line">урок</p>
+                </div>
                 <p class="value">13:30 - 14:15</p>
             </div>
     `;
@@ -206,19 +343,31 @@ function second() {
     const scheduleCont = document.querySelector('.schedule-cont');
     scheduleCont.innerHTML = `
             <div class="lesson">
-                <p class="num">1</p>
+                <div class="time">
+                    <p class="first">1</p>
+                    <p class="line">пара</p>
+                </div>
                 <p class="value">08:30 - 09:50</p>
             </div>
             <div class="lesson">
-                <p class="num">2</p>
+                <div class="time">
+                    <p class="first">2</p>
+                    <p class="line">пара</p>
+                </div>
                 <p class="value">10:05 - 11:25</p>
             </div>
             <div class="lesson">
-                <p class="num">3</p>
+                <div class="time">
+                    <p class="first">3</p>
+                    <p class="line">пара</p>
+                </div>
                 <p class="value">11:50 - 13:05</p>
             </div>
             <div class="lesson">
-                <p class="num">4</p>
+                <div class="time">
+                    <p class="first">4</p>
+                    <p class="line">пара</p>
+                </div>
                 <p class="value">13:20 - 14:40</p>
             </div>
     `;
